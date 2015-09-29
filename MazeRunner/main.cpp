64f15995dfd0d4e690b402b2a868c3173513c45e
Alexander Rivera <El_Chule@Tree-Club-Studios.local>
2015-09-29 14:43:04 -0400

    Initial Commit

---
+++ MazeRunner/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MazeRunner
+//
+//  Created by Alexander Rivera on 9/29/15.
+//  Copyright (c) 2015 Alexander Rivera. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
